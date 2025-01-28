a582b22d8895eeb608936c423885d65bb50831d4
peachparfait <peachparfait10@gmail.com>
2025-01-29 01:51:18 +0900

    アニメーション作成

--- animate1.c
+++ animate1.c
@@ -5,8 +5,17 @@ int main(void) {
   struct color c1 = { 30, 255, 0 };
   struct color c2 = { 255, 0, 0 };
   int i;
+  int easing = 0;
+  for(i = 20; i > 0; --i) {
+    easing = i*i*i/20;
+    img_clear(); img_fillu(c1, 100+easing, 85, 20);img_fille(c1, 150+easing, 85, 20);img_fillc(c1, 197+easing,85, 20); img_write();
+  }
   for(i = 0; i < 20; ++i) {
     img_clear(); img_fillu(c1, 100, 85, 20);img_fille(c1, 150, 85, 20);img_fillc(c1, 197,85, 20); img_write();
+  }
+  for(i = 0; i < 20; ++i) {
+    easing = i*i*i/20;
+    img_clear(); img_fillu(c1, 100+easing, 85, 20);img_fille(c1, 150+easing, 85, 20);img_fillc(c1, 197+easing,85, 20); img_write();
   }
   return 0;
 }
